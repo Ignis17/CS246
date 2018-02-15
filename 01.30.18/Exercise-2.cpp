0fb27bd46d8c1830216bfb7e0f3ca9997b3663dd
Ignis17 <joel.turbi@student.mec.cuny.edu>
2018-02-15 04:27:48 -0500

    hello

--- 01.30.18/Exercise-2.cpp
+++ 01.30.18/Exercise-2.cpp
@@ -6,27 +6,53 @@
 // Due by: 02/01/2018
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cassert> // Needed for assert member function.
 using namespace std;
 
-int main ()
+int integers()
 {
   int total=1;
+  int counter = 1;
+  string line;
+  ofstream out;
+  out.open("numbers.txt");
 
-  while(total <= 20)
+  while(total <= 20 && counter <= 1000)
   {
     int number;
+
     cout << "Please enter an integer: \n";
     cin >> number;
 
     if(number%3==0)
     {
-
       int divisible = 0;
       divisible +=number;
-      cout << "\n" << divisible << " is divisible by 3.\n\n";
+      out << divisible << "\n";
       total++;
     }
+    counter++;
   }
+  out.close();
+  ifstream in;
+  in.open("numbers.txt");
 
+  if (in.is_open())
+  {
+    cout << "\nThese are the first 20 set of numbers divisible by 3: \n";
+    while (getline (in,line) )
+    {
+      cout << line << '\n';
+    }
+    in.close();
+  }
+}
+
+int main()
+{
+  integers();
   return 0;
 }

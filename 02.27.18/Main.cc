25174cff8216755e11b3ad1518e59953db271f76
Ignis17 <joel.turbi@student.mec.cuny.edu>
2018-02-28 22:39:51 -0500

    Final touches to points class assignment

--- 02.27.18/Main.cc
+++ 02.27.18/Main.cc
@@ -1,4 +1,5 @@
-// This program 
+// This program calculates the distance between two points as well as
+// its midpoint.
 // Name: Joel Turbi
 // Course: CS246
 // Assignment: 2A & 2B
@@ -7,21 +8,32 @@
 #include <cstdlib>
 #include <cmath> // Needed for math library.
 #include "Point.h"
+#define NDEBUG
+#include <cassert>
 using namespace std;
 
 // This function calculates the distance between two points.
 float Distance(const float& x,const float& y)
 {
-	float result; // Stores the the distance between x & y.
-	result = sqrt((x*x)+(y*y)); // calculates the square root of the sum of x-squared & y-squared.
+	float result, mid; // Stores the the distance between x & y and mid stores the midpoint between the two points.
+	result = sqrt((x*x)+(y*y)); // Calculates the square root of the sum of x-squared & y-squared.
+	// Pre-processor: Checks to see that result is greater than or equal to 0(zero).
+	// Post-processor: If true it returns an error, else the program executes as follows.
+	assert(result >= 0);
+	// Calculates the midpoint of x and y.
+	mid = (x + y)/2;
+	// Pre-processor: Checks to see if midpoint is greater than or equal to 0(zero).
+	// Post-processor: If true it returns an error, else the program executes as follows.
+	assert(mid >= 0);
+
 	// Displays the distance between the two points.
 	cout << "The distance between " << x << " and " << y << " is " << (result) << ".\n";
+	cout << "The midpoint of " << x << " and " << y << " is " << (mid) << ".\n";
 }
 
 int main()
 {
 	Point p1(3,4);
-	Point p2(1,2);
 	Distance(p1.get_x(),p1.get_y());
 	return 0;
 }
--- 02.27.18/Point.h
+++ 02.27.18/Point.h
@@ -1,7 +1,8 @@
-// This program
+// This program calculates the distance between two points as well as
+// its midpoint.
 // Name: Joel Turbi
 // Course: CS246
-// Assignment:
+// Assignment: 2A & 2B
 // Due: 03-01-18
 #ifndef POINT_H
 #define POINT_H
@@ -9,21 +10,21 @@
 class Point
 {
 public:
-  //default if no values provided
-  Point(double init_x=0, double init_y=0)
+  // Default if no values provided.
+  Point(double init_x = 0, double init_y = 0)
   {
     x = init_x;
     y = init_y;
 	}
-  //shift a point by dx and dy values
+  // Shift a point by dx and dy values
   void shift(double dx, double dy)
   {
     x = x + dx;
-    y =y + dy;
+    y = y + dy;
 	}
-  //return the value of x
+  // Return the value of x
   double get_x() const{return x;}
-  //return the value of y
+  // Return the value of y
   double get_y() const{return y;}
 private:
   double x;

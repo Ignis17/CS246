95275e591c9a49e230800215f87656c4d9c81d52
Ignis17 <joel.turbi@student.mec.cuny.edu>
2018-02-28 19:28:45 -0500

    Final touches to Clock class

--- 02.27.18/Clock.h
+++ 02.27.18/Clock.h
@@ -1,6 +1,11 @@
+// This program simulates a clock and displays the current time.
+// Name: Joel Turbi
+// Course: CS246
+// Assignment: Clock class
+// Due: 03-01-18
 #ifndef CLOCK_H
 #define CLOCK_H
-//#define NDEBUG
+#define NDEBUG
 #include <sstream>
 #include <iomanip>
 #include <cassert>
@@ -11,21 +16,24 @@ private:
   int hour;
   int minutes;
 public:
-  Clock():hour(0),minutes(0){}
-  void SetHour(int value)
+  Clock():hour(0),minutes(0){} // Default constructor.
+  void SetHour(int value) // Hour setter function.
   {
-    if(value>=0&&value<=23)
+    if(value >= 0 && value <= 23)
     {
-      // Pre-procesor:
-      // Post-procesor: 
-      assert(value>=0 && value <=23);
+      // Pre-processor: Checks for value validation.
+      // Post-processor: If true it performs the following assignment, else it returns an error.
+      assert(value >= 0 && value <= 23);
       hour = value;
     }
   }
-  void SetMinutes(int value)
+  void SetMinutes(int value) // Minutes setter function.
   {
-    if(value>=0&&value<=59)
+    if(value >= 0 && value <= 59)
     {
+      // Pre-processor: Checks for value validation.
+      // Post-processor: If true it performs the following assignment, else it returns an error.
+      assert(value >= 0 && value <= 59);
       minutes = value;
     }
   }
--- 02.27.18/Main.cc
+++ 02.27.18/Main.cc
@@ -1,10 +1,16 @@
+// This program simulates a clock and displays the current time.
+// Name: Joel Turbi
+// Course: CS246
+// Assignment: Clock class
+// Due: 03-01-18
 #include "Clock.h"
 #include <iostream>
-#include <ctime>
+#include <ctime> // Needed for current time.
 using namespace std;
 
 void Display()
 {
+  // current date/time based on current system.
   time_t now = time(0);
   tm *ltm = localtime(&now);
 

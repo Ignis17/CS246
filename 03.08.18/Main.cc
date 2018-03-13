e1b0e57137506ebcc8ac69bfa6dd3572a719ce87
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-03-12 23:18:57 -0400

    Final changes to inheritance class

--- 03.08.18/Main.cc
+++ 03.08.18/Main.cc
@@ -21,23 +21,46 @@ void First()
 void Second()
 {
 	Point p1(3,4);
-	p1.SetX(19);
+	//p1.SetX(19); // Had to be commented out in order for third instance to work.
 	cout << p1.GetX() << endl;
 
 	/*What happens and why?
 		- An error message is displayed, stating the following:
-			Void Point::SetX(double) ' is protected. This error was due to
+			void Point::SetX(double) ' is protected. This error was due to
 			SetX() being defined as protected and not being accessable by
 			Base function whose data is declared as private.*/
 }
-// 
+//
 void Third()
+{
+	//Circle c1(4,6.02,1.03);
+	Circle c1;
+	// Modified cordinates for Cirle Class
+	c1.SetX(2);
+	c1.SetY(2);
+	/* What happens?
+		- An error message is displayed, stating that:
+			protected members from base class is declared as protected and it is
+			protected within this context c1.SetX(2); Therefore cordinate values
+			cannot be set. */
+	cout << "Radius: " << c1.GetRadius();
+	cout << " X: " << c1.GetX() << " Y: " << c1.GetY() << endl;
+
+
+
+
+	/*What happens?
+		- An error message is displayed, stating the following:
+		 	Point class is not an accessible base of Circle. Both getters for
+			X and Y values are not accessible.  */
+}
 int main()
 {
 	//First(); // Commented because it is no longer needed for Point.h
 					  // illustrations.
 	//Second(); // Commented since it was only used for demonstration
 						 // purposes.
-	Third();
+	Third(); //Commented since it was only used for demonstration
+						 // purposes
 	return 0;
 }
--- 03.08.18/Point.h
+++ 03.08.18/Point.h
@@ -4,6 +4,7 @@
 // Course: CS246
 // Assignment: 2A & 2B
 // Due: 03-13-18
+
 #ifndef POINT_H
 #define POINT_H
 #include <iostream>
@@ -19,7 +20,9 @@ public:
   double GetX() const {return x;}
   // Return the value of y
   double GetY() const {return y;}
-protected:
+//protected: // In order to permit main program to use SetX() and SetY() functions
+           // the protected specifier for setter functions can be commented out.
+           // making it public instead. 
   // Sets value for x.
   void SetX(double x){this->x = x;}
   // Sets value for y.
@@ -28,13 +31,37 @@ private:
   double x,y; // Compact declaration
 };
 
-class Circle: protected Point
+// The difference in access:
+// Class Circle: public Point - When using the access specifier as public base
+//                              class gives access to its public member functions,
+//                              as well as the ones declared as protected(setters).
+//
+//                            - Public and protected members of base class will
+//                              be accessible in devired class as public and
+//                              protected respectively. In protected mode, the
+//                              public and protected members of base class
+//                              becomes protected members of derived class.
+//
+//                           - In this example having a public access specifier
+//                             would be more suited.
+//
+// Class Circle: private Point - In private mode the public and protected
+//                               members of base class become private members
+//                               of derived class.
+//
+// Class Circle: protected Point - Neither Point base class or its public member
+//                                 functions are accessible to Circle (child class).
+
+
+class Circle: public Point
 {
 public:
+  Circle(double radius, double x, double y):Point(x,y){this->radius = radius;}
   // Just set the radius for now.
   Circle(double radius = 1){this->radius =  radius;}
   // Gets the radius
   double GetRadius() const {return radius;}
+
 private:
   double radius;
 };

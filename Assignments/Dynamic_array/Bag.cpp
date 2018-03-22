37f2cfdaef0e95128cdc1c11f92166a6eb43eec2
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-03-22 02:28:11 -0400

    Last changes to Dynamic array class & Set Class

--- Assignments/Dynamic_array/Bag.cpp
+++ Assignments/Dynamic_array/Bag.cpp
@@ -1,10 +1,11 @@
 // This program is an implementation of the bag class. This is an intro to
 // the container class. A container class is a class that can hold a collection
-// of items.
+// of items. This assignment modifies the Bag class with a derived set class
+// to replace static arrays with dynamic arrays.
 // Author: Joel Turbi
 // Course: CS246
-// Assignment: Bag Class
-// Due: 03/20/2018
+// Assignment: Dynamic Array & Set Class
+// Due: 03/22/2018
 
 #include "Bag.h"
 #include "Bag.cc"
@@ -14,18 +15,21 @@ int main(){
   Bag x; // second Bag object.
   Set u; // Sets object.
   bag_type value;
-  int limit=1;
+  int limit = 1;
 
-  while(limit <= 20){b.Insert(limit);limit++;}
+  while(limit <= 20){b.Insert(limit);limit++;} // Assigns a value to the bag
+                                               // class. Which is then used
+                                               // by the set class.
+  // Display bag info
   cout << "First Bag" << endl;
   cout << b.Size()<< " elements in the bag" << endl;
-
   cout << endl << "Second Bag " << endl;
   x = b; // second bag a copy of first.
-  cout << x.Size() << " elements in bag" << endl;
+  cout << x.Size() << " elements in bag" << "\n\n";
 
-  cout << endl;
-  u.Union(x.GetData(), b.GetData());
-  u.Intersection(x.GetData(), b.GetData());
+  // Display set info
+  u.Union(x.GetData(), b.GetData()); // calls Union function from the set class.
+  u.Intersection(x.GetData(), b.GetData()); // calls Intersection funtion from
+                                            // the set class.
   return 0;
 }

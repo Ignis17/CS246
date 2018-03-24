d8b0d13be93f54c6604f58c81c9e91bde302bc5c
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-03-24 05:00:35 -0400

    Link list exercise

--- Exercises/Link_List_Nodes_exercise/Node.cc
+++ Exercises/Link_List_Nodes_exercise/Node.cc
@@ -3,12 +3,22 @@
 using namespace std;
 
 int main(){
+  //
   Node *p, *q, *r;
+
+  // With no arguments, we will use both default values, so p's data will be
+  // set to zero and p's link will be set to NULL:
   p = new Node;
+  // We can explicitly set the data part of q's node to 4.9, and use the
+  // default argument of NULL for q's link_field, like this:
   q = new Node(4.9);
+  // We can create a new node for r to point to with data of 1.6 and a link
+  // field that points to the same node as p:
   r = new Node(1.6,p);
 
-  cout << (*q).Data() << endl;
-  p=q;
+  //
+  cout << q->Data() << " " << q->Link() << endl;
+  // 
+  p = q;
   return 0;
 }
--- Exercises/Link_List_Nodes_exercise/Node.h
+++ Exercises/Link_List_Nodes_exercise/Node.h
@@ -4,32 +4,26 @@
 
 class Node{
 public:
+  // TYPEDEF
   typedef double value_type;
-
   // CONSTRUCTOR
-  Node(const value_type& data = value_type(), Node* link = NULL){}
-
-  // Member function to set the data field
-  void  Set_Data(const value_type& new_data){
-    data_field = new_data;
-  }
-  // Member function to set  the link field
-  void Set_Link(Node* new_link){
-    link_field = new_link;
-  }
-  // Member function to retrieve the current data
-  value_type Data() const
+  Node(const value_type& init_data = value_type(),Node* init_link = NULL)
   {
-    return data_field;
+    data_field = init_data;
+    link_field = init_link;
   }
-  // Member fucntions to retrieve current link data
-  const Node* Link() const
-  {
-    return link_field;
-  }
-  Node* Link(){
-    return link_field;
-  }
+  // Member function to set the data and link fields:
+  void Set_Data(const value_type& new_data){data_field = new_data;}
+  void Set_Link(Node* new_link){link_field = new_link;}
+  // Constant member function to retrieve the current data
+  value_type Data()const{return data_field;}
+  // Two slightly different functions to retrieve current link:
+  const Node* Link() const{return link_field;}
+  Node* Link(){return link_field;}
+  size_t listLength(const Node* head_ptr)
+  // Precondition: head_ptr is the head pointer of a linked list.
+  // Postcondition: The value returned is the number of nodes in the linked list.
+  {}
 private:
   value_type data_field;
   Node* link_field;

75e9a21944b0c3e014c8a37c278aff1824ab1a5b
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-03-26 20:41:29 -0400

    Fixed files

--- Exercises/Assert_examples/Assert.Example.cpp
+++ Exercises/Assert_examples/Assert.Example.cpp
@@ -1,21 +1,20 @@
 //Demonstrating example program for array
 //out-of-bound assignment
 #include <iostream>
- 
+using namespace std;
+
 int main()
 {
-     using namespace std;
-     int a[10] = {0,1,2,3,4,5,6,7,8,9};
 
-     //modify the values
- 
-     for(int i=0; i<10; i++)
-        a[i] = a[i+1];    //a[9] <- a[10], error!!
+  int a[10] = {0,1,2,3,4,5,6,7,8,9};
 
-     //print out values
-     cout << "myarray elements ";
-     for(int i=0;i<10;i++)
-        cout << ' ' << a[i];
-     cout << "\n";
-     return 0;
+  for(int i=0; i<10; i++)
+    a[i] = a[i+1];    //a[9] <- a[10], error!!
+
+  cout << "myarray elements ";
+
+  for(int i=0;i<10;i++)
+    cout << ' ' << a[i];
+  cout << "\n";
+  return 0;
 }

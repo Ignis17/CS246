7e87a8e2c0c9562f43fc89f251a41411cb782c5f
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-04-05 02:39:06 -0400

    Modification to list class

--- Exercises/Link_List_Nodes_exercise/List_demo/List.cc
+++ Exercises/Link_List_Nodes_exercise/List_demo/List.cc
@@ -6,129 +6,32 @@
 // Assignment: Node Class / List Class
 // Created: 03/29/2018
 
+#include "List.h" // Provides List.h members.
 
-#include <cstdlib> // Provides NULL
-#include <iostream>
-#include <cassert> // Provides assert
-#include "List.h" // Uses List header file
-
-using namespace std;
-
-List::List()
+int main()
 {
-  head = NULL;
-  curr = NULL;
-  temp = NULL;
-}
+  List *test = new List; // Pointer object List.
 
-void List::AddNode(value_type addData)
-{
-  nodePtr n = new node;
-  n->next = NULL;
-  n->data = addData;
+  value_type *a = new value_type[SIZE]; //  Pointer array declared.
+                                       // Assigns size of dynamic array.
+  test->NumberGenerator(a); // Calls function to generate random numbers
+                            // in array between 1 and 100.
+  test->Sort(a); // Sorts numbers in array in ascending order.
 
-  if(head != NULL)
-  {
-    curr = head;
-    while(curr->next != NULL)
-    {
-      curr = curr->next;
-    }
-    curr->next = n;
-  }
-  else
-  {
-    head = n;
-  }
-
-}
-void List::DeleteNode(value_type delData)
-{
-  nodePtr delPtr = NULL;
-  temp = head;
-  curr = head;
-
-  while(curr != NULL && curr->data != delData)
-  {
-    temp = curr;
-    curr = curr->next;
-  }
-  if(curr == NULL)
-  {
-    cout << delData << " was not in the list.\n";
-    delete delPtr;
-  }
-  else
-  {
-    delPtr = curr;
-    curr = curr->next;
-    temp->next = curr;
-    delete delPtr;
-    cout << "The value " << delData <<  " was deleted.\n";
-  }
-}
-
-void List::PrintList()
-{
-  curr = head;
-  while(curr != NULL)
-  {
-      cout << curr->data << endl;
-      curr = curr->next;
-  }
-}
-
-void List::Sort(value_type a[], size_type SIZE)
-{
-  bool swapped = false;
-
-  do
-  {
-    swapped = false; // reset before each scan
-
-    for (value_type i = 1; i < SIZE; i++) // start at 1 and compare with previous
-    {
-      assert(i < SIZE); // Checks that i does not exceed the value of SIZE.
-
-      if (a[i-1] > a[i])  // move bigger values along, use "<" for descending
-      {
-        int temp; // Temporary int variable to hold value of index.
-        temp = a[i];
-        a[i] = a[i-1];
-        a[i-1] = temp;
-        swapped = true; // swapped is assigned a true value.
-      }
-    }
-  }while(swapped);
-}
-
-void List::NumberGenerator(value_type arr[], size_type SIZE)
-{
-
-  for (value_type i=0;i < SIZE;i++)
+  for(value_type i = 0; i < SIZE; i++)
+  // Precondition: Inserts random values from 1 to 100 into list.
   {
     assert(i < SIZE); // Checks that i does not exceed the value of SIZE.
-
-    bool check; //  bool variable to check or number is already used
-    int n; // variable to store the number
-
-    do
-    {
-      // Generates random number between 1 and 100 and stores it in local
-      // variable.
-      n = rand()% 100 + 1;
-      // check or number is already used:
-      check = true;
-
-      for (value_type j = 0; j < i; j++)
-      {
-        if (n == arr[j]) // if number is already used.
-        {
-          check=false; // Set check to false.
-          break; // no need to check the other elements of arr[].
-        }
-      }
-    }while(!check); // loop until new, unique number is found.
-    arr[i] = n; // store the generated number in the array.
+    test->AddNode(a[i]); // Inserts values into list.
   }
-};
+
+  // Prints / displays values in list.
+  std::cout << "These are the numbers in this list:\n";
+  test->PrintList(); // Calls print member function & displays values.
+
+  delete [] test, a; // deallocates pointer array.
+  a = nullptr; // Pointer is no longer referencing anything.
+  test = nullptr;
+
+  return 0;
+}
--- Exercises/Link_List_Nodes_exercise/List_demo/List.h
+++ Exercises/Link_List_Nodes_exercise/List_demo/List.h
@@ -6,14 +6,18 @@
 // Assignment: Node Class / List Class
 // Created: 03/29/2018
 
-
 #ifndef LIST_H
 #define LIST_H
 
+#include <cstdlib> // Provides NULL
+#include <iostream>
+#include <cassert> // Provides assert
+
 typedef int value_type;
 typedef size_t size_type;
-
 const size_type SIZE = 12; // Global const variable.
+
+using namespace std;
 
 class List
 {
@@ -29,11 +33,133 @@ private:
   nodePtr temp;
 
 public:
-  List();
-  void AddNode(value_type addData);
-  void DeleteNode(value_type delData);
-  void PrintList();
-  void Sort(value_type a[], size_type SIZE);
-  void NumberGenerator(value_type arr[], size_type SIZE);
+  List()
+  {
+    head = NULL;
+    curr = NULL;
+    temp = NULL;
+  }
+  void AddNode(value_type addData)
+  {
+    nodePtr n = new node;
+    n->next = NULL;
+    n->data = addData;
+
+    if(head != NULL)
+    {
+      curr = head;
+      while(curr->next != NULL)
+      {
+        curr = curr->next;
+      }
+      curr->next = n;
+    }
+    else
+    {
+      head = n;
+    }
+  }
+  void DeleteNode(value_type delData)
+  {
+    nodePtr delPtr = NULL;
+    temp = head;
+    curr = head;
+
+    while(curr != NULL && curr->data != delData)
+    {
+      temp = curr;
+      curr = curr->next;
+    }
+    if(curr == NULL)
+    {
+      cout << delData << " was not in the list.\n";
+      delete delPtr;
+    }
+    else
+    {
+      delPtr = curr;
+      curr = curr->next;
+      temp->next = curr;
+      delete delPtr;
+      cout << "The value " << delData <<  " was deleted.\n";
+    }
+  }
+
+  void Sort(value_type *arr)
+  {
+    value_type *a = arr;
+
+    for(int i = 0; i < SIZE; i++)
+    {
+      for(int j=0; j < (SIZE-i-1);j++)
+      {
+        if(*(a+j) > *(a+(j+1)))
+        {
+          int temp = *(a+j);
+          *(a+j) = *(a+(j+1));
+          *(a+(j+1)) = temp;
+        }
+      }
+    }
+  }
+
+  void PrintList()
+  {
+    curr = head;
+    while(curr != NULL)
+    {
+      cout << curr->data << endl;
+      curr = curr->next;
+    }
+  }
+  // void Sort(value_type *a)
+  // {
+  //   for(int *first = a, *swapped = a, *last = a + SIZE; !(last - first < 2 ); first = a, last = swapped)
+  //   {
+  //     swapped = a;
+  //     while ( ++first != last)
+  //     {
+  //       if (*(first - 1) > *first)
+  //       {
+  //         swapped = first;
+  //         int tmp = *first;
+  //         *first = *(first - 1);
+  //         *(first - 1) = tmp;
+  //       }
+  //     }
+  //   }
+  // }
+  void NumberGenerator(value_type *&a)
+  {
+    delete [] a;
+    a = new value_type[SIZE];
+
+    for (value_type i=0;i < SIZE;i++)
+    {
+      assert(i < SIZE); // Checks that i does not exceed the value of SIZE.
+
+      bool check; //  bool variable to check or number is already used
+      int n; // variable to store the number
+
+      do
+      {
+        // Generates random number between 1 and 100 and stores it in local
+        // variable.
+        n = rand()% 100 + 1;
+        // check or number is already used:
+        check = true;
+
+        for (value_type j = 0; j < i; j++)
+        {
+          if (n == a[i]) // if number is already used.
+          {
+            check=false; // Set check to false.
+            break; // no need to check the other elements of arr[].
+          }
+        }
+      }while(!check); // loop until new, unique number is found.
+      a[i] = n; // store the generated number in the array.
+    }
+  }
 };
 #endif

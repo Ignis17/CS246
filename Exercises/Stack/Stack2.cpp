5f5a19276cd16c95dba46cc9c2151f1b4fc8985d
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-04-14 02:32:42 -0400

    Stack - Assignment

--- Exercises/Stack/Stack2.cpp
+++ Exercises/Stack/Stack2.cpp
@@ -1,25 +1,29 @@
-+// This program illustrates the Stack STL function.
-+// Name: Joel Turbi
-+// Course: CS246
-+// Created: 04/12/2018
-+
-+#include <iostream>
-+#include <stack>
-+
-+using namespace std;
-+
-+int main()
-+{
-+  stack<string> words;
-+
-+  words.push("Madam,");
-+  words.push(" I'm" );
-+  words.push("Adam.");
-+
-+  while(!words.empty())
-+  {
-+    std::cout << words.top() << std::endl;
-+    words.pop();
-+  }
-+  return 0;
-+}
+// This program illustrates the Stack STL function.
+// Name: Joel Turbi
+// Course: CS246
+// Created: 04/12/2018
+
+#include <iostream>
+#include <stack>
+#include <fstream>
+
+int main()
+{
+  std::stack<std::string> words;
+  std::string text;
+  std::ifstream file;
+  file.open("war-madlib.txt");
+
+  while(file>>text)
+  {
+    words.push(text);
+  }
+  file.close();
+
+  while(!words.empty())
+  {
+    std::cout << words.top() << std::endl;
+    words.pop();
+  }
+  return 0;
+}

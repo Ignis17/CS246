6f09fde65767ef7f9ee664176b01c0716e883050
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-04-29 18:59:19 -0400

    Made changes to Template example

--- Exercises/Template/Template.cc
+++ Exercises/Template/Template.cc
@@ -6,7 +6,7 @@
 #include <iostream>
 
 template <class T>
-T fillArray(double arr[], int size,T value)
+T fillArray(T arr[],T size,T value)
 {
   for(int i = 0; i < size;i++)
   {
@@ -30,9 +30,10 @@ T max(T val1, T val2)
 
 int main()
 {
-   int array[7]={2.0,4,5,9,12,14,16};
+  int array[]={2,4,5,9,12,14,16};
+  int size = sizeof(array) / sizeof(array[0]);
 
-  std::cout << "Position: " << fillArray(array,7,2) << std::endl;
-  std::cout << "Position: " << fillArray(array,7,6) << std::endl;
+  std::cout << "Position: " << fillArray(array,size,2) << std::endl;
+  std::cout << "Position: " << fillArray(array,size,6) << std::endl;
   std::cout << "Max value: " << max(4,7) << std::endl;
 }
